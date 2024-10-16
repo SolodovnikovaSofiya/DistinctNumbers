e7a99eb2ac603b85b4a1eacbc92f96da3b341cbf
217117 <solodovnikovasofiya98@gmail.com>
2024-10-16 16:57:35 +0300

    готовый код

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,25 @@
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        ArrayList numbers = new ArrayList<>();
+        try (Scanner scanner = new Scanner(System.in)) {
+            List<Integer> uniqueNumbers = new ArrayList<>();
 
-        System.out.println("Введите числа:");
-        while (true) {
-            int num = scanner.nextInt();
-            if (num == 0) {
-                break;
+            System.out.println("Введите числа (введите 0 для завершения):");
+            while (true) {
+                int num = scanner.nextInt();
+                if (num == 0) {
+                    break;
+                }
+                if (!uniqueNumbers.contains(num)) {
+                    uniqueNumbers.add(num);
+                }
             }
-            if (!numbers.contains(num)) {
-                numbers.add(num);
-            }
+
+            System.out.println("Несовпадающие числа:");
+            uniqueNumbers.forEach(System.out::println);
         }
-        System.out.println("Несовпадающие числа:");
-        for (Object number : numbers) {
-            System.out.println(number);
-        }
-        //закрываем scanner
-        scanner.close();
     }
 }
